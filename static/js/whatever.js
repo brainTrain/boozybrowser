8c192ef9f6950c14e0a94fb8c2a8a9d0ef8ead86
Brain Train <abductee@gmail.com>
2013-10-17 21:21:41 -0700

    Update whatever.js

--- static/js/whatever.js
+++ static/js/whatever.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
-
+    /*
     $('button').click(function() {
         var postDisShiz = {'wahhhhhhh': 'bahh'};
         $.ajax({
             type: 'POST',
             data: JSON.stringify(postDisShiz),
-            url: 'http://logs.marie.uni.loggly.net/inputs/ac074846-eb05-4b59-b81b-3d92deac6cd6',
+            url: '',
         });
         
 
     });
-
+    */
     
 });

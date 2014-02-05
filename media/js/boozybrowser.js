bdb2d4038428c05b82fed61732abdf189afdaa59
braintrain <abductee@gmail.com>
2014-02-04 23:59:52 -0800

    we now have boozy type and boozy buttons
    * Plus some convenience functions for randomz

--- media/js/boozybrowser.js
+++ media/js/boozybrowser.js
@@ -1,21 +1,38 @@
 (function(window, document, $){
-    var boozy = { 
+    window.boozy = { 
         init: function() {
             $('.button').click(function() {
                 $('.button').removeClass('pressed');
                 $(this).addClass('pressed');
             });
 
-            boozy.keys();
-            boozy.buttons();
+            boozy.keys.init();
+            boozy.buttons.init();
         },
-        abc: 'abcdefghijklmnopqrstuvwxyz'.split(''),
-        keys: function() {
-            var keyCounter = 0,
-                randomInterval = Math.floor((Math.random()*17)+8);
-            $('.typin textarea').keyup(function(event) {
-
-                if(keyCounter == randomInterval){
+        boundedRandomInterval: function(min, max) {
+            return Math.floor(Math.random()*(max-min+1)+min);
+        },
+        posNeg: function() {
+            return Math.random() < 0.555555 ? -1 : 1;
+        },
+        randDirection: function(randSize) { 
+            return boozy.posNeg() * Math.floor((Math.random()*randSize)+1);
+        },
+        keys: {
+            init: function() {
+                $('.typin textarea').keyup(boozy.keys.typer);
+                boozy.keys.setRandomInterval();
+            },
+            abc: 'abcdefghijklmnopqrstuvwxyz'.split(''),
+            keyCounter: 0,
+            howDrunk: 3,
+            howSober: 10,
+            randomInterval: 10,
+            setRandomInterval: function() {
+                boozy.keys.randomInterval = boozy.boundedRandomInterval(boozy.keys.howDrunk, boozy.keys.howSober);
+            },
+            typer: function() {
+                if(boozy.keys.keyCounter == boozy.keys.randomInterval){
                     var $textField = $(this),
                         randomBurst = Math.floor((Math.random()*3)+1),
                         randomLetters = '',
@@ -23,46 +40,41 @@
 
                     for(j=0; j<randomBurst; j++) {
                         randCount = Math.floor((Math.random()*26));
-                        randomLetters += boozy.abc[randCount];
+                        randomLetters += boozy.keys.abc[randCount];
                     }
-
                     var newBoozyType = boozyType + randomLetters; 
                     $textField.val(newBoozyType);
 
-                    randomInterval = Math.floor((Math.random()*17)+8);
-                    keyCounter = 0;
+                    boozy.keys.setRandomInterval();
+                    boozy.keys.keyCounter = 0;
                 }
-                
-                keyCounter ++;
-            });
+                boozy.keys.keyCounter ++;
+            }
         },
-        _posNeg: function() {
-            return Math.random() < 0.555555 ? -1 : 1;
-        },
-        _randDirection: function(randSize) { 
-            return boozy._posNeg() * Math.floor((Math.random()*randSize)+1);
-        },
-        buttons: function() {
-            var $buttons = $('.button, button, .btn');
-            $buttons.mouseover(function() {
+        buttons: {
+            init: function() {
+                $('.button, button, .btn').mouseover(boozy.buttons.boozyMotions);
+            },
+            boozyMotions: function() {
                 var $button = $(this),
                     randSize = 15;
 
                 if($button.attr('id') == 'submit') {
                     randSize = 85;
-                } 
+                }
 
                 $button.animate({
-                    "right": "+=" + boozy._randDirection(randSize) + "px", 
-                    "top": "+=" + boozy._randDirection(randSize) + "px", 
+                    "right": "+=" + boozy.randDirection(randSize) + "px", 
+                    "top": "+=" + boozy.randDirection(randSize) + "px", 
                 },{
                     duration: 250,
                 });
-            });
+            }
         }
     };
 
     $(document).ready(function() {
         boozy.init();     
     });
+
 })(window, document, jQuery);

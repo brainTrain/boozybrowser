a44664b5f9b0919ee546181f17c735c3d5b26a8c
braintrain <abductee@gmail.com>
2014-02-15 19:59:13 -0800

    replace bool switch with dropdown change..
    * gotta make init not stack

--- media/js/boozybrowser.js
+++ media/js/boozybrowser.js
@@ -8,7 +8,7 @@
             - buttons: very slight motion.. noticible but very much usuable (buttons should stay within their region desite number of hovers)
             - lean: no lean
             - focus: no focus
-        * getting drunk: definitely noticiable but not annoying 
+        * I'm fine: definitely noticiable but not annoying 
             - keys: noticible type-o's (user should now noticed but still not really be annoyed by them)
             - buttons: noticible motion.. still usable but close to annoying
             - lean: very slight intermittent lean.. kinda like small flashes of rolls
@@ -18,7 +18,7 @@
             - buttons: quite dynamic motion, should border on frustrating, perhaps random keys are more difficult to chase down than others
             - lean: lean should be in full effect, the page should sway back and forth constantly but not by too much
             - focus: focus now throbs in and out.  The max blur should still be fairly  readable
-        * drunk drunk: holy shit I can barely use this webpage
+        * wooo!: holy shit I can barely use this webpage
             - keys: type-o's should happen with every word, in fact a fair number of words should have more than one injected type-o
             - buttons: buttons should now be close to impossible to click.. perhaps some random buttons are easier to click
             - lean: the sway should be constant and quite large.. the user should be getting sea sick at this point
@@ -49,10 +49,10 @@
         init: function() {
             // only initialize after the menu is loaded
             boozy._menu.init(function() {
-                boozy.buttons.init();
-                boozy.focus.init();
-                boozy.lean.init();
-                boozy.keys.init();
+                //boozy.buttons.init();
+                //boozy.focus.init();
+                //boozy.lean.init();
+                //boozy.keys.init();
             });
 
             $('.button').click(function() {
@@ -61,17 +61,61 @@
             });
 
         },
+        _howDrunkHandler: function(event) {
+            var $drunkDrop =  $(event.target).closest('.drunk-level'),
+                $controlContainer = $drunkDrop.parents('.control'),
+                controlId = $controlContainer.attr('id'),
+                drunkLevel = $drunkDrop.val(),
+                drunkObject = {
+                    "controlId": controlId,
+                    "drunkLevel": drunkLevel
+                };
+            boozy._howDrunk(drunkObject); 
+        },
+        _howDrunk: function(drunkObject) {
+            if(drunkObject) {
+                console.log(drunkObject);
+                if(drunkObject.drunkLevel === 'sober') {
+                    console.log('sober');
+                    // be sober
+                    boozy[drunkObject.controlId].stop();
+
+                } else if(drunkObject.drunkLevel === 'buzzed') {
+                    console.log('drunk');
+                    // be drunk
+                    boozy[drunkObject.controlId].init();
+                    
+                } else if(drunkObject.drunkLevel === 'im-fine') {
+                    // be drunk
+                    boozy[drunkObject.controlId].init();
+
+                } else if(drunkObject.drunkLevel === 'drunk') {
+                    // be drunk
+                    boozy[drunkObject.controlId].init();
+
+                } else if(drunkObject.drunkLevel === 'wooo') {
+                    // be drunk
+                    boozy[drunkObject.controlId].init();
+
+                } else if(drunkObject.drunkLevel === 'blackout') {
+                    // be drunk
+                    boozy[drunkObject.controlId].init();
+                }
+            }
+        },
         // all the boozy shit
         lean: {
             _leanIntervalId: undefined,
+            _transitionClass: 'transition-ease-out',
             _howDrunk: 2,
             _howFast: 2000,
             init: function() {
-                var $page = $(boozy._pageSelectors),
-                    transitionClass = 'transition-ease-out';
+                console.log($(''))
+                console.log('lean init');
+                var $page = $(boozy._pageSelectors);
                  
-                if(!$page.hasClass(transitionClass)) {
-                    $page.addClass(transitionClass);
+                if(!$page.hasClass(boozy.lean._transitionClass)) {
+                    $page.addClass(boozy.lean._transitionClass);
                 }
 
                 boozy.lean._goHomeYoureDrunk($page);
@@ -80,6 +124,7 @@
                 }, boozy.lean._howFast);
             },
             stop: function() {
+                console.log('lean stop');
                 clearInterval(boozy.lean._leanIntervalId);
                 $(boozy._pageSelectors)
                     .removeClass('rotate-' + boozy.lean._howDrunk)
@@ -104,6 +149,7 @@
         },
         focus: {
             _focusIntervalId: undefined,
+            _transitionClass: 'transition-ease-out',
             _currentBlur: 1,
             _blurMin: 0,
             _blurMax: 6,
@@ -111,7 +157,7 @@
             init: function() {
                 var $page = $(boozy._pageSelectors);
                 
-                $page.addClass('transition-ease-out');
+                $page.addClass(boozy.focus._transitionClass);
 
                 boozy.focus._focusIntervalId = setInterval(function(){
                     boozy.focus._goHomeYoureDrunk($page);
@@ -120,7 +166,8 @@
             stop: function() {
                 clearInterval(boozy.focus._focusIntervalId);
 
-                $(boozy._pageSelectors).removeClass('blur-' + boozy.focus._currentBlur);
+                $(boozy._pageSelectors)
+                    .removeClass('blur-' + boozy.focus._currentBlur);
 
                 boozy.focus._currentBlur = 1;
                 boozy.focus._blurDirection = 'pos';
@@ -161,7 +208,8 @@
             _howSober: 10,
             _randomInterval: 10,
             init: function() {
-                $(boozy._typingSelectors).on(boozy.keys._boozySpace, boozy.keys._goHomeYoureDrunk);
+                $(boozy._typingSelectors)
+                    .on(boozy.keys._boozySpace, boozy.keys._goHomeYoureDrunk);
                 boozy.keys._setRandomInterval();
             },
             _setRandomInterval: function() {
@@ -196,7 +244,8 @@
             _howFast: 250,
             _boozyNamespace:'mouseover.boozy-space',
             init: function() {
-                $(boozy._buttonSelectors).on(boozy.buttons._boozyNamespace, boozy.buttons._goHomeYoureDrunk);
+                $(boozy._buttonSelectors)
+                    .on(boozy.buttons._boozyNamespace, boozy.buttons._goHomeYoureDrunk);
             },
             _goHomeYoureDrunk: function() {
                 var $button = $(this),
@@ -212,7 +261,8 @@
                 });
             },
             stop: function() {
-                $(boozy._buttonSelectors).off(boozy.buttons._boozyNamespace);
+                $(boozy._buttonSelectors)
+                    .off(boozy.buttons._boozyNamespace);
             }
         },
         // utility functions 
@@ -239,9 +289,14 @@
 
                 $menuContainer.append(boozy._menu._menuHTML.join(''));
 
-                $('.control', $menuContainer).on('click', boozy._menu.handleControlClicks);
-                $('.hide', $menuContainer).on('click', boozy._menu.handleHideClicks);
-                $('.show', $menuContainer).on('click', boozy._menu.handleShowClicks);
+                $('.drunk-level', $menuContainer)
+                    .on('change', boozy._howDrunkHandler);
+                $('.control', $menuContainer)
+                    //.on('click', boozy._menu.handleControlClicks);
+                $('.hide', $menuContainer)
+                    .on('click', boozy._menu.handleHideClicks);
+                $('.show', $menuContainer)
+                    .on('click', boozy._menu.handleShowClicks);
 
                 callBack();
             },
@@ -280,55 +335,79 @@
                 }
             },
             _menuHTML: [
-            '<div class="boozy-menu">',
-                '<h2 class="title font float-left">Boozy Browsin</h2>',
-                '<div class="hide font float-right cursor-pointer">x</div>',
-                '<div class="clear-both"></div>',
-                '<div class="controls-container display-inline-block">',
-                    '<div id="lean" class="control cursor-pointer display-inline-block">',
-                        '<div class="title font float-left">',
-                            'Lean',
-                        '</div>',
-                        '<div class="switch float-right">',
-                            '<div class="on option float-left"></div>',
-                            '<div class="off option float-left deactivated"></div>',
-                        '</div>',
-                    '</div>',
-                    '<div class="clear-both"></div>',
-                    '<div id="focus" class="control cursor-pointer display-inline-block">',
-                        '<div class="title font float-left">',
-                            'Focus',
-                        '</div>',
-                        '<div class="switch float-right">',
-                            '<div class="on option float-left"></div>',
-                            '<div class="off option float-left deactivated"></div>',
-                        '</div>',
-                    '</div>',
-                    '<div class="clear-both"></div>',
-                    '<div id="buttons" class="control cursor-pointer display-inline-block">',
-                        '<div class="title font float-left">',
-                            'Buttons',
-                        '</div>',
-                        '<div class="switch float-right">',
-                            '<div class="on option float-left"></div>',
-                            '<div class="off option float-left deactivated"></div>',
-                        '</div>',
-                    '</div>',
-                    '<div class="clear-both"></div>',
-                    '<div id="keys" class="control cursor-pointer display-inline-block">',
-                        '<div class="title font float-left">',
-                            'Keys',
-                        '</div>',
-                        '<div class="switch float-right">',
-                            '<div class="on option float-left"></div>',
-                            '<div class="off option float-left deactivated"></div>',
-                        '</div>',
-                    '</div>',
-                    '<div class="clear-both"></div>',
-                '</div>',
-                '<div class="clear-both"></div>',
-                '<div class="show font float-right cursor-pointer fade-out">open</div>',
-            '</div>'
+'<div class="boozy-menu">',
+    '<h2 class="title font float-left">Boozy Browsin</h2>',
+    '<div class="hide font float-right cursor-pointer">x</div>',
+    '<div class="clear-both"></div>',
+    '<div class="controls-container display-inline-block">',
+        '<div id="lean" class="control cursor-pointer display-inline-block">',
+            '<div class="title font float-left">',
+                'Lean',
+            '</div>',
+            '<div class="switch float-right">',
+                '<select class="drunk-level float-left">',
+                    '<option value="sober">sober</option>',
+                    '<option value="buzzed">buzzed</option>',
+                    '<option value="im-fine">I\'m fine</option>',
+                    '<option value="drunk">drunk</option>',
+                    '<option value="wooo">wooo!</option>',
+                    '<option value="blackout">blackout</option>', 
+                '</select>',
+            '</div>',
+        '</div>',
+        '<div class="clear-both"></div>',
+        '<div id="focus" class="control cursor-pointer display-inline-block">',
+            '<div class="title font float-left">',
+                'Focus',
+            '</div>',
+            '<div class="switch float-right">',
+                '<select class="drunk-level float-left">',
+                    '<option value="sober">sober</option>',
+                    '<option value="buzzed">buzzed</option>',
+                    '<option value="im-fine">I\'m fine</option>',
+                    '<option value="drunk">drunk</option>',
+                    '<option value="wooo">wooo!</option>',
+                    '<option value="blackout">blackout</option>', 
+                '</select>',
+            '</div>',
+        '</div>',
+        '<div class="clear-both"></div>',
+        '<div id="buttons" class="control cursor-pointer display-inline-block">',
+            '<div class="title font float-left">',
+                'Buttons',
+            '</div>',
+            '<div class="switch float-right">',
+                '<select class="drunk-level float-left">',
+                    '<option value="sober">sober</option>',
+                    '<option value="buzzed">buzzed</option>',
+                    '<option value="im-fine">I\'m fine</option>',
+                    '<option value="drunk">drunk</option>',
+                    '<option value="wooo">wooo!</option>',
+                    '<option value="blackout">blackout</option>', 
+                '</select>',
+            '</div>',
+        '</div>',
+        '<div class="clear-both"></div>',
+        '<div id="keys" class="control cursor-pointer display-inline-block">',
+            '<div class="title font float-left">',
+                'Keys',
+            '</div>',
+            '<div class="switch float-right">',
+                '<select class="drunk-level float-left">',
+                    '<option value="sober">sober</option>',
+                    '<option value="buzzed">buzzed</option>',
+                    '<option value="im-fine">I\'m fine</option>',
+                    '<option value="drunk">drunk</option>',
+                    '<option value="wooo">wooo!</option>',
+                    '<option value="blackout">blackout</option>', 
+                '</select>',
+            '</div>',
+        '</div>',
+        '<div class="clear-both"></div>',
+    '</div>',
+    '<div class="clear-both"></div>',
+    '<div class="show font float-right cursor-pointer fade-out">open</div>',
+'</div>'
             ]
         }
     };

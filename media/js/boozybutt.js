2fdae730c3c371d66f7bfa8c631abd7050d5dcbb
braintrain <abductee@gmail.com>
2014-09-20 13:12:30 -0400

    just a lil bit of cleanup

--- media/js/boozybutt.js
+++ media/js/boozybutt.js
@@ -2,14 +2,10 @@
     dis guy will consume the BoozyBrowser object to show how it's done
 */
 
-(function($, BoozyBrowser){
+(function($){
     var boozy = { 
-        _currentDrunkLevel: 'sober',
-        _drunkLevels: ['sober', 'buzzed', 'im-fine', 'drunk', 'wooo', 'blackout'],
-        _drunkArray: ['buzzed', 'im-fine', 'drunk', 'wooo', 'blackout'],
         init: function() {
-            // only initialize after the menu is loaded
-            boozy._menu.init();
+            boozy.menu.init();
 
             $('.button').click(function() {
                 $('.button').removeClass('pressed');
@@ -18,7 +14,7 @@
 
         },
         // menu control rendering/event handling
-        _menu: {
+        menu: {
             init: function() {
                 var $boozyMenuTemplate = $('#boozy-menu-template');
                 $('html').append($boozyMenuTemplate.html());
@@ -26,12 +22,11 @@
                 // init teh angularz by hand! 
                 angular.bootstrap($('.boozy-menu'), ['boozyDemo'])
             }
-        },
+        }
     };
 
     $(document).ready(function() {
         boozy.init();
-        window.soBoozy = boozy;
     });
 
-})(jQuery, window.BoozyBrowser);
+})(jQuery);
